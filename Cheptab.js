00db2563d1c28b74c30146e1a0ec3be24f8a5d58
GiaoThay <86911329+GiaoThay@users.noreply.github.com>
2022-02-25 21:04:12 +0700

    Update Cheptab.js

--- Cheptab.js
+++ Cheptab.js
@@ -7,3 +7,16 @@ Sub cheptab()
     ActiveSheet.Select
     ActiveSheet.Copy Before:=Sheets(2)
 End Sub
+Sub dienso2()
+'
+' Macro9 Macro
+'
+
+'
+    Range("A9").Value = "6"
+     Range("A14").Value = "7"
+      Range("A19").Value = "8"
+     Range("A24").Value = "9"
+     Range("A29").Value = "10"
+     
+End Sub
